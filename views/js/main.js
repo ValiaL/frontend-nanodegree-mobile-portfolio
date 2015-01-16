266a7ba990d1f54690d3569243e809c38e882f72
ValiaL <wahia83@gmail.com>
2015-01-16 14:55:03 +0200

    Fix main.js and index.html after reverting mistake

--- views/js/main.js
+++ views/js/main.js
@@ -503,8 +503,12 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var ScrollPos = document.body.scrollTop / 1250;
+  var sinScrollPos =  Math.sinScrollPos(ScrollPos);
+  var cosScrollPos = Math.cos(ScrollPos);
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    //var phase = Math.sin(ScrollPos + (i % 5));
+    var phase = (sinScrollPos*Math.cos(i%5)) + (cosScrollPos*Math.sin(i%5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
