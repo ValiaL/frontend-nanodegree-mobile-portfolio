694c14e3d6713a7a419d3cb401fd514e1cddc6bc
ValiaL <wahia83@gmail.com>
2015-01-16 16:16:57 +0200

    Add forgotten parenthesis

--- views/js/main.js
+++ views/js/main.js
@@ -451,7 +451,7 @@ var resizePizzas = function(size) {
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
 
-    var randomPizzaContainerAll = document.querySelectorAll(".randomPizzaContainer";
+    var randomPizzaContainerAll = document.querySelectorAll(".randomPizzaContainer");
     var pizzaContainerLength = randomPizzaContainerAll.length;
     var dx = determineDx(randomPizzaContainerAll[0], size);
     var newwidth = (randomPizzaContainerAll[0].offsetWidth + dx) + 'px';
